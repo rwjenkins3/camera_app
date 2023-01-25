c36613f65a36ce1577c7f207524b0a069a99c42f
Rob Jenkins <rwjenkins@gmail.com>
2023-01-25 10:57:22 -0500

    First pass camera

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
+import React, { useState, useRef, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
 
 export default function App() {
+
+  const [ cameraType, setCameraType ] = useState(Camera.Constants.Type.back);
+  const [ flashMode, setFlashMode ] = useState(Camera.Constants.FlashMode.off);
+  const [ hasCameraPermission, setHasCameraPermission ] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, []);
+
+  if(hasCameraPermission === false) {
+    return (
+      <Text>No Camera Permissions</Text>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={cameraType}
+        ref={cameraRef}
+        flashMode={flashMode}
+      >
+      </Camera>
     </View>
   );
 }
@@ -13,8 +39,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  camera: {
+    flex: 1
+  }
 });
