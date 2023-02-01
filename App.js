163219598a8d9c1998508928d057fe9d8e50cc49
Rob Jenkins <rwjenkins@gmail.com>
2023-02-01 11:02:27 -0500

    Fixed preview

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import Feather from '@expo/vector-icons/Feather';
 
@@ -9,12 +9,12 @@ import Feather from '@expo/vector-icons/Feather';
 export default function App() {
 
   const [ cameraType, setCameraType ] = useState(CameraType.front);
+  const [ camera, setCamera ] = useState(null);
   const [ flashMode, setFlashMode ] = useState(Camera.Constants.FlashMode.off);
   const [ hasCameraPermission, setHasCameraPermission ] = useState(null);
   const [ image, setImage ] = useState(null);
   
-  // const cameraRef = useRef(null);
-  let cameraRef;
+  let cameraRef = useRef();
 
   useEffect(() => {
     (async () => {
@@ -23,12 +23,14 @@ export default function App() {
     })();
   }, []);
 
-  const takePicture = () => {
+  const takePicture = async () => {
+    console.log("In takePicture");
     if(cameraRef) {
+      console.log("...with cameraRef");
       try {
-        const data = cameraRe
+        const data = await cameraRef.current.takePictureAsync();
         console.log(data);
-        setImage(data.uri);
+        setImage(data);
       } catch (error) {
         console.log(error);
       }
@@ -42,22 +44,40 @@ export default function App() {
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <Camera
-        style={styles.camera}
-        type={cameraType}
-        ref={ref => (cameraRef = ref)}
-        flashMode={flashMode}
-      >
-      </Camera>
-      <View style={styles.controlContainer}>
-        <Pressable onPress={takePicture}>
-          <Feather name="circle" style={styles.cameraButton} size={48} color='white' />
-        </Pressable>
+  if(!image) {
+    return (
+      <View style={styles.container}>
+        <Camera
+          style={styles.camera}
+          type={cameraType}
+          ref={cameraRef}
+          flashMode={flashMode}
+        >
+        </Camera>
+        <View style={styles.controlContainer}>
+          <Pressable onPress={takePicture}>
+            <Feather name="circle" style={styles.cameraButton} size={48} color='white' />
+          </Pressable>
+        </View>
       </View>
-    </View>
-  );
+    );
+  } else {
+    return(
+      <View style={styles.container}>
+        <Image source={{uri: image.uri}} style={styles.camera} />
+        <View style={styles.controlContainer}>
+          <Pressable onPress={() => setImage(null)}>
+            <Text style={styles.ctlText}>Save Image</Text>
+          </Pressable>
+          <Pressable onPress={() => setImage(null)}>
+            <Text style={styles.ctlText}>Discard Image</Text>
+          </Pressable>
+        </View>
+      </View>
+    );
+
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -74,9 +94,15 @@ const styles = StyleSheet.create({
 
   },
 
+  ctlText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: '500'
+  },
+
   controlContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: 'black',
     padding: 12
